43ef9f223f550a303138d7c92b1eea61523d827f
VIC LAM <viclam@VICs-MacBook-Pro.local>
2016-12-05 11:44:15 -0500

    FancyPAnts!

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -1,2 +1,14 @@
+require_relative './fancy_dance.rb'
 require_relative './class_methods_module.rb'
 require_relative './dance_module.rb'
+
+class Dancer
+  include FancyDance::InstanceMethods         # Instance Methods
+  extend FancyDance::ClassMethods    # Class Methods
+
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+  end
+end
